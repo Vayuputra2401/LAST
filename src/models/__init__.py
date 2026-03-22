38539383ee8464f946cf2947b4153e274d1b3e4a
Pathikreet <pathikreetofficial@gmail.com>
2026-03-23 03:33:48 +0530

    update

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,15 +3,23 @@
 from .shiftfuse_zero import (
     ShiftFuseZero,
     ShiftFuseZeroLate,
+    ShiftFuseZeroB4,
+    ShiftFuseZeroX,
     build_shiftfuse_zero,
     build_shiftfuse_zero_late,
+    build_shiftfuse_zero_b4,
+    build_shiftfuse_zero_x,
     ZERO_VARIANTS,
 )
 
 __all__ = [
     'ShiftFuseZero',
     'ShiftFuseZeroLate',
+    'ShiftFuseZeroB4',
+    'ShiftFuseZeroX',
     'build_shiftfuse_zero',
     'build_shiftfuse_zero_late',
+    'build_shiftfuse_zero_b4',
+    'build_shiftfuse_zero_x',
     'ZERO_VARIANTS',
 ]
